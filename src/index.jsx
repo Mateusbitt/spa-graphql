b6743667d7d56303005eb237fbd00f0ad8dcc994
Breno Mazieiro <breno.mazieiro@gmail.com>
2020-10-02 19:06:10 +0100

    will add globalStyles to project

--- src/index.jsx
+++ src/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { render } from 'react-dom'
 import { ApolloProvider } from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom'
+import { GlobalStyles } from 'themes'
 import { client } from './apolloConfig'
 import App from './components/App'
 
 const renderApp = () => {
   return (
     <ApolloProvider client={client}>
+      <GlobalStyles />
       <BrowserRouter>
         <App />
       </BrowserRouter>
