a8e5e99773bede8b001d8b6a46a3c15c75a89b5d
Breno Mazieiro <breno.mazieiro@gmail.com>
2020-10-02 19:55:11 +0100

    will change laguage and persiste it on localStorage

--- src/components/molecules/Content/index.jsx
+++ src/components/molecules/Content/index.jsx
@@ -1,12 +1,31 @@
 import React from 'react'
+import styled from 'styled-components'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router-dom'
-import { Block } from 'components'
+import { Block, Button } from 'components'
+import { usePersistedState } from 'hooks'
 
-const Content = () => (
-  <Block>
-    <Block>Home</Block>
-    <Link to="/login">Login</Link>
-  </Block>
-)
+const Wrapper = styled(Block)`
+  display: flex;
+  flex-direction: row;
+
+`
+
+const Content = () => {
+  const [lang, setLang] = usePersistedState('language', JSON.parse(localStorage.getItem('language')) || 'enUS')
+  const { i18n, t } = useTranslation()
+  const changeSelect = () => {
+    i18n.changeLanguage(lang === 'ptBR' ? 'enUS' : 'ptBR')
+    setLang(lang === 'ptBR' ? 'enUS' : 'ptBR')
+  }
+
+  return (
+    <Wrapper>
+      <Block>{t('molecules.Content.Home')}</Block>
+      <Button text="Trocar Lingua" onClick={() => changeSelect()} />
+      <Link to="/login">Login</Link>
+    </Wrapper>
+  )
+}
 
 export { Content }
