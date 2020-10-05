2a9aaab1461c503ea5f1b1d401bf6123baa751d6
Breno Mazieiro <breno.mazieiro@gmail.com>
2020-10-05 18:14:08 +0100

    will add molecule LoginForm

---
+++ src/components/molecules/Forms/LoginForm/index.jsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { ADForm, ADFormItem, ADButton, ADInput, ADInputPassword, ADCheckbox } from 'components'
+
+const LoginForm = ({ onFinish, onFinishFailed }) => {
+  return (
+    <ADForm
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <ADFormItem
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <ADInput />
+      </ADFormItem>
+
+      <ADFormItem
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <ADInputPassword />
+      </ADFormItem>
+
+      <ADFormItem name="remember" valuePropName="checked">
+        <ADCheckbox>Remember me</ADCheckbox>
+      </ADFormItem>
+
+      <ADFormItem>
+        <ADButton type="primary" danger htmlType="submit">
+          Submit
+        </ADButton>
+      </ADFormItem>
+    </ADForm>
+  )
+}
+
+LoginForm.propTypes = {
+  onFinish: PropTypes.func.isRequired,
+  onFinishFailed: PropTypes.func.isRequired,
+}
+
+export { LoginForm }
