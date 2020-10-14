1b9dd0f3c667f1f105e72918c41f863a44eea593
Breno Mazieiro <breno.mazieiro@gmail.com>
2020-10-14 21:11:35 +0100

    will bugfix example

--- src/environment/index_example.js
+++ src/environment/index_example.js
@@ -1,4 +1,4 @@
-export const HTTP_API_URL = 'http://api.local.plathanus.com.br'
-export const WS_API_URL = 'ws://api.local.plathanus.com.br'
+export const API_URL = 'http://api.local.plathanus.com.br'
+export const WS_URL = 'ws://api.local.plathanus.com.br'
 export const CLIENT = 'plathanus'
 export const DEBUG = 'true'
